deeebd2d97916dcc45178711dd3a0ae405ba70de
Ibrahim Benaaddi <benaaddiibrahim@gmail.com>
2025-12-11 01:20:24 +0100

    build menu for app

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+echo "*************************************\n";
+echo "*****Welcome*To*Task*Tracker*CLI*****\n";
+echo "*************************************\n";
+echo "1->Read All your tasks\n"; // we can select all tasks in-progress and after apply filte / mark task 
+echo "2->Add new task\n";
+echo "3->Update task by id\n"; //first find task and then show it for check
+echo "4->Delete task by id\n"; //first find task and then show it for check 
+echo "5->Delete all tasks\n";
