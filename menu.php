a0935c4856a024a7efbbb3007ed3624bbdaaf285
Ibrahim Benaaddi <benaaddiibrahim@gmail.com>
2025-12-11 22:59:19 +0100

    update menu.php     i add function askTheuse for more dynamic code in main fun & filterTask     i add $range for dynamic and easy to modify

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <?php
 require_once "./App/list.php";
 
-// build functions for handleinput of user
-
-function handleInput(string $value)
+// build functions for handleinput and user choices
+// i use the range for more dynamic for update in future
+function handleInput(string $value, int $range)
 {
     try {
         if (empty($value)) {
@@ -15,9 +15,9 @@ function handleInput(string $value)
         if (!filter_var($value, FILTER_VALIDATE_INT)) {
             throw new Exception('Plz enter an integer');
         }
-        settype($value, "int");
-        if ($value < 1 || $value > 6) {
-            throw new Exception('Plz choise from the range 1~>6');
+        $value = (int) $value;
+        if ($value < 1 || $value > $range) {
+            throw new Exception("Plz choise from the range 1~>$range");
         }
         return $value;
     } catch (Exception $err) {
@@ -26,10 +26,47 @@ function handleInput(string $value)
     }
 }
 
+function askTheUser(int $range): int
+{
+    while (true) {
+        echo "\nplz enter your choise 1~>$range : ";
+        $choice = trim(fgets(STDIN));
+        $choice = handleInput($choice, $range);
+        if ($choice) {
+            return $choice;
+        }
+    };
+}
+
+function filterTask()
+{
+    $range = 4;
+    echo "\nPlz choose a number to apply the filter\n";
+    echo "1->All the list\n";
+    echo "2->todo\n";
+    echo "3->in-progress\n";
+    echo "4->done\n";
+
+    $mode = askTheUser($range);
+
+    if ($mode === 1) {
+        return null;
+    }
+    if ($mode === 2) {
+        return "todo";
+    }
+    if ($mode === 3) {
+        return "in-progress";
+    }
+    if ($mode === 4) {
+        return "done";
+    }
+}
+
 function handleChoice(int $choice)
 {
     if ($choice === 1) {
-        mainlist();
+        mainlist(filterTask());
         return;
     }
 
@@ -37,6 +74,7 @@ function handleChoice(int $choice)
 
 function main(): void
 {
+    $range = 7;
     while (true) {
         if (!file_exists("./database.json")) {
             file_put_contents("database.json", json_encode([]));
@@ -46,20 +84,14 @@ function main(): void
         echo "*****Welcome*To*Task*Tracker*CLI*****\n";
         echo "*************************************\n";
         echo "1->Read All your tasks\n"; // we can select all tasks in-progress and after apply filte / mark task 
-        echo "2->Add new task\n";
-        echo "3->Update task by id\n"; //first find task and then show it for check
-        echo "4->Delete task by id\n"; //first find task and then show it for check 
-        echo "5->Delete all tasks\n";
-        echo "6->Exit\n";
+        echo "2->Find the task by ID\n";
+        echo "3->Add new task\n";
+        echo "4->Update task by id\n"; //first find task and then show it for check
+        echo "5->Delete task by id\n"; //first find task and then show it for check 
+        echo "6->Delete all tasks\n";
+        echo "7->Exit\n";
 
-        while (true) {
-            echo "plz enter your choise 1~>6 : ";
-            $choice = trim(fgets(STDIN));
-            $choice = handleInput($choice);
-            if ($choice) {
-                break;
-            }
-        };
+        $choice = askTheUser($range);
 
         if ($choice === 6) {
             break;
