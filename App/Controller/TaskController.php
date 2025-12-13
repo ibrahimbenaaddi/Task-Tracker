5f7761776b3e19370213c9d70bb7273e9fc52ae6
Ibrahim Benaaddi <benaaddiibrahim@gmail.com>
2025-12-13 13:31:20 +0100

    update the structure of the project     i move some functions in main the right place

--- App/Controller/TaskController.php
+++ App/Controller/TaskController.php
@@ -51,4 +51,68 @@ class TaskController
             echo "\n" . $err->getMessage() . "\n\n";
         }
     }
+
+    public static function update(int $id)
+    {
+        $range = 4;
+
+        echo "\nPlz choose a number to update\n";
+        echo "1->Update Task\n";
+        echo "2->Start the Task (statud = in-progress)\n";
+        echo "3->Update status to done\n";
+        echo "4->Exit\n";
+
+        $choice = self::askTheUser($range);
+
+        switch ($choice) {
+            case $choice === 1:
+                while (true) {
+                    echo "\nEnter the new Task : ";
+                    $description = trim(fgets(STDIN));
+                    if (!empty($description)) {
+                        self::updateTask($id, $description);
+                        return;
+                    }
+                    echo ("Plz insert a task to Add");
+                }
+            case $choice === 2:
+                self::updateTask($id, null, "in-progress");
+                return;
+            case $choice === 3:
+                self::updateTask($id, null, "done");
+                return;
+            case $choice === 4:
+                return;
+        }
+    }
+
+    public static function delete(int $id)
+    {
+        $range = 2;
+
+        echo "\nPlz choose a number from Option :\n";
+        echo "1->Delete Task that have ID : $id \n";
+        echo "2->Exit\n";
+
+        $choice = self::askTheUser($range);
+
+        switch ($choice) {
+            case $choice === 1:
+                while (true) {
+                    echo "\nAre you sure Want to delete your Task The ID : $id yes/no : ";
+                    $answer = trim(fgets(STDIN));
+                    if (strtolower($answer) === "yes") {
+                        self::deleteByID($id);
+                        return;
+                    }
+
+                    if (strtolower($answer) === "no") {
+                        return;
+                    }
+                    echo "\nAnswer By yes/no\n\n";
+                }
+            case $choice === 2:
+                return;
+        }
+    }
 }
--- App/View/view.php
+++ App/View/view.php
@@ -1,8 +1,16 @@
 <?php
+
 namespace App\View;
+
+require_once __DIR__ . "/../handler.php";
+
+use App\Handler;
 
 trait View
 {
+
+    use Handler;
+
     private const Thead = " ___________________________________________________________________________________________\n|  id  |       descritpion       |    status    |       createdAt     |       updatedAt     |\n|------|-------------------------|--------------|---------------------|---------------------|\n";
 
     private const Tfooter = "|______|_________________________|______________|_____________________|_____________________|\n\n";
@@ -35,5 +43,52 @@ trait View
     public static function findTasks(array $task): void
     {
         echo "\nID : {$task['id']},\nDescription : {$task['description']},\nStatus : {$task['status']},\nCreatedAt : {$task['createdAt']},\nUpdatedAt : {$task['updatedAt']}\n\n";
+    }
+    
+    public static function Hint()
+    {
+        echo "\nHint : use Read all option in menu to see the right Task\n";
+        echo "--------------------------------------------------------";
+        echo "\nPlz enter the ID of the task : ";
+    }
+
+    public static function askTheUser(int $range): int
+    {
+        while (true) {
+            echo "\nplz enter your choice 1~>$range : ";
+            $choice = trim(fgets(STDIN));
+            $choice = self::handleInput($choice, $range);
+            if ($choice) {
+                return $choice;
+            }
+        };
+    }
+
+    public static function filterTask()
+    {
+        $range = 4;
+
+        echo "\nPlz choose a number to apply the filter\n";
+        echo "1->All the list\n";
+        echo "2->todo\n";
+        echo "3->in-progress\n";
+        echo "4->done\n";
+
+        $mode = self::askTheUser($range);
+
+        switch ($mode) {
+            case $mode === 1:
+                self::index();
+                return;
+            case $mode === 2:
+                self::showByStatus("todo");
+                return;
+            case $mode === 3:
+                self::showByStatus("in-progress");
+                return;
+            case $mode === 4:
+                self::showByStatus("done");
+                return;
+        }
     }
 }
